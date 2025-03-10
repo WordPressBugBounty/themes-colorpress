055ba51dd31c0c875ec0856d4d93a690250729fe
Sean Murphy <sean@iamseanmurphy.com>
2025-03-10 14:17:41 +0000

    12.5

--- colorpress/functions.php
+++ colorpress/functions.php
@@ -3,7 +3,7 @@
 /**
  * Define Theme Version
  */
-define( 'COLORPRESS_THEME_VERSION', '12.4' );
+define( 'COLORPRESS_THEME_VERSION', '12.5' );
 
 function colorpress_css() {
 	$parent_style = 'gradiant-parent-style';
